e06ee32d59e889ca1e82fa5c3c1ac10da82c802a
Bianca Fagundes <biasaur@gmail.com>
2024-07-16 18:25:22 -0300

    feat: created empty task page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { PlusCircle } from 'phosphor-react'
+import { PlusCircle, ClipboardText } from 'phosphor-react'
 import { Header } from './components/Header'
 
 import './global.css'
@@ -15,9 +15,27 @@ function App() {
         <form className={styles.toDoForm}>
           <input type='text' name='to-do' placeholder='Adicione uma nova tarefa'/>
           <button type='submit'>
-            Criar <PlusCircle size={16} />
+            Criar <PlusCircle size={20} />
           </button>
         </form>
+      </section>
+
+      <section className={styles.taskList}>
+        <div className={styles.header}>
+          <div>
+            <p>Tarefas criadas</p>
+            <span>0</span>
+          </div>
+          <div>
+            <p>Concluídas</p>
+            <span>0</span>
+          </div>
+        </div>
+        <div className={styles.list}>
+          <ClipboardText size={70} />
+          <p>Você ainda não tem tarefas cadastradas</p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
       </section>
     </>
   )
